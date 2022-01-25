897fb38e9a921441cd65be1919f3e1dcd282a699
Dayana Osipova <elista111@yandex.ru>
2022-01-25 22:28:14 +0300

    task_final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,22 +34,20 @@
 // }
 
 
+function sendRequest(name, phone, address, goods, sum) {
+    let data = {client: {}, order: {address, sum}, goods: []};
 
-function sendRequest (name, address, goods, sum) {
-    
-	let data = {client:"Иван +7(987)65-43-210", 
-	order: {address: 'ул. Ленина, дом 2, 4 подъезд, 5 этаж, кв 53', sum: 900},
-	goods: [{title: 'Пицца', count: 2}]
-    };
-    
     let countOfGoods = goods.length;
+    for (let i = 0; i < countOfGoods; i += 1){
+      let a = {title: goods[i].title, count: goods[i].count};
+       data.goods.push(a);
+         }
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
+     data.client = name + " " + phone;
+    data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, " + "кв " + address.flat;
     data.order.sum = sum;
-
+    
+    
     let jsonData = JSON.stringify({data});
 
     return jsonData;
